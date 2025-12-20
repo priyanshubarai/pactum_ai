e39cac397123c42efac0860e6abda6d0fe62f05f
priyanshubarai <priyanshubarai45@gmail.com>
2025-12-20 14:19:52 +0530

    main to head

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -6,7 +6,7 @@ import { Badge } from "@/components/ui/badge"
 
 export function Sidebar() {
     return <div
-        className=" h- bg-[#0f0f0f] w-85 border p-5 border-[#252525] fixed">
+        className=" h-screen bg-[#0f0f0f] w-85 border p-5 border-[#252525] fixed">
             <div className=" flex flex-col gap-4 p-2 mb-13  items-baseline  ">
                 <p className="font-bold text-2xl"> Pactum Dashboard </p>
                 <AnimatedBadge
